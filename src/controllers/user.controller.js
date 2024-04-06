259da68b82e63dd7f0e5c9e8756dfe6ff93e5149
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-04-06 11:01:25 -0300

    connection with mongodb atlas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,22 @@
-const e = require("express");
+const userService = require("../services/user.service");
 
-const create = (req, res) => {
+const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
         res.status(400).send({ message: 'All fields required' });
     }
 
+    const user = await userService.create(req.body);
+
+    if(!user){
+        return res.status(400).send({ message: 'Error creating User' });
+    }
+
     res.status(201).send({
         message: 'User created successfully',
         user: {
+            id: user._id,
             name,
             username,
             email,
